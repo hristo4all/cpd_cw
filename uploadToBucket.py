c03d2d3bb1c58c9a271f6f0e571443aa07a762cd
Hristo Ivanov <55858676+hristo4all@users.noreply.github.com>
2022-04-14 09:51:53 +0100

    Add files via upload

--- uploadToBucket.py
+++ uploadToBucket.py
@@ -3,17 +3,28 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 import glob
+import time
 
-images = glob.glob("images/*.jpg") + glob.glob("images/*.png")
-FOLDER_NAME = "images/"
-s3_client = boto3.client('s3')
+def upload_file(file_name='images/image1.jpg', bucket='s2030512' , object_name=None):
 
-try:
-  #response = s3_client.upload_file(file_name, bucket, object_name)
-  for filename in images:
-    object_name = os.path.basename(filename)
-    print("Putting %s as %s" % (filename,object_name))
-    s3_client.upload_file(filename, bucket, object_name)
+    images = glob.glob("/home/ec2-user/environment/images/*.jpg") + glob.glob("/home/ec2-user/environment/images/*.png")
+    FOLDER_NAME = "images/"
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        #response = s3_client.upload_file(file_name, bucket, object_name)
+        for filename in images:
             
-except ClientError as e:
-  logging.error(e)
+            object_name = os.path.basename(filename)
+            print("Putting %s as %s" % (filename,object_name))
+            s3_client.upload_file(filename, bucket, object_name)
+            time.sleep(30);
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+upload_file()
