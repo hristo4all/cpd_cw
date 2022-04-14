025784651d9f65247463561763509ee5caeff568
Ubuntu <ubuntu@ip-172-31-85-204.ec2.internal>
2022-04-14 18:25:57 +0000

    revisited upload script

--- uploadToBucket.py
+++ uploadToBucket.py
@@ -1,34 +1,38 @@
 import logging
-# import boto3
-# from botocore.exceptions import ClientError
+import boto3
+from botocore.exceptions import ClientError
 import os
 import glob
 import time
+import json
 
-
-def upload_file(file_name="images/image1.jpg", bucket='s2030512', object_name=None):
-
+def upload_file(bucket='s2030512', object_name=None):
+    
     images = glob.glob("images/*.jpg") + glob.glob("images/*.png")
     FOLDER_NAME = "images/"
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = os.path.basename(file_name)
+
     if not images:
         print("images is empty")
-    print(images)
 
     # Upload the file
-    # s3_client = boto3.client('s3')
-    # try:
-    # response = s3_client.upload_file(file_name, bucket, object_name)
-    for filename in images:
+    s3_client = boto3.client('s3', region_name='us-east-1')
+    try:
+        for filename in images:
 
-        # object_name = os.path.basename(filename)
-        print("Putting %s as %s" % (filename, object_name))
-    # s3_client.upload_file(filename, bucket, object_name)
-    # time.sleep(30)
-    # except ClientError as e:
-    # logging.error(e)
+            object_name = os.path.basename(filename)
+            print("Putting %s as %s" % (filename, object_name))
+            s3_client.upload_file(filename, bucket, object_name)
+            message = {"key":object_name,"bucket":bucket}
+            sns_client = boto3.client('sns',region_name='us-east-1')
+            sns_response = sns_client.publish(
+                    TopicArn='arn:aws:sns:us-east-1:481685060162:topic_s2030512',
+                    Message=json.dumps({'default': json.dumps(message)}),
+                    MessageStructure='json'
+                    )
+            #print(response)
+            time.sleep(30)
+    except ClientError as e:
+        logging.error(e)
     return False
     return True
 
